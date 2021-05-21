a81505c14695a8749437a804323cbbf32a8f6b0f
renatsayf <renatsayf@gmail.com>
2021-05-21 15:22:06 +0500

    Scree2Fragment.kt speech recognizer has been implemented

--- app/src/main/java/com/renatsayf/mycheatsheets/ui/screen2/Scree2Fragment.kt
+++ app/src/main/java/com/renatsayf/mycheatsheets/ui/screen2/Scree2Fragment.kt
@@ -1,20 +1,23 @@
 package com.renatsayf.mycheatsheets.ui.screen2
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.renatsayf.mycheatsheets.R
 import com.renatsayf.mycheatsheets.databinding.Scree2FragmentBinding
+import java.util.*
 
 private const val recordAudioRequestCode: Int = 3698
 
@@ -39,15 +42,56 @@ class Scree2Fragment : Fragment(R.layout.scree2_fragment)
             // TODO Runtime permission Step 4
             if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
             {
-                activityResultLauncher.launch(Manifest.permission.RECORD_AUDIO)
+                recordAudioPermission.launch(Manifest.permission.RECORD_AUDIO)
+                return@setOnClickListener
+            }
+            runSpeechRecognizer()
+        }
+
+        binding.resultTV.onItemClickListener = object : AdapterView.OnItemClickListener
+        {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long)
+            {
+                val text = (p1 as TextView).text
+                return
+            }
+
+        }
+    }
+
+    private fun runSpeechRecognizer()
+    {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            action = RecognizerIntent.ACTION_RECOGNIZE_SPEECH
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "Говорите в микрофон...")
+        }
+        speechRecognizer.launch(intent)
+    }
+
+    private val speechRecognizer = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        val data = result.data
+        data?.let {
+            val listExtra = it.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (!listExtra.isNullOrEmpty())
+            {
+                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, listExtra)
+                binding.resultTV.apply {
+                    setAdapter(adapter)
+                    showDropDown()
+                }
             }
         }
+        return@registerForActivityResult
     }
 
 
     // TODO Runtime permission Step 3
-    private val activityResultLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()){ isGranted ->
+    private val recordAudioPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
         if (isGranted) Toast.makeText(requireContext(), "Отлично. Теперь можно распознавать речь...", Toast.LENGTH_LONG).show()
         return@registerForActivityResult
     }
